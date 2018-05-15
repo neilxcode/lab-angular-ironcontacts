d2ad97f655164425046abe20563c41f9fd19b147
neilxcode <developros@icloud.com>
2018-05-15 12:14:32 -0400

    Iteration 2

--- starter-code/src/app/contact-list/contact-list.component.ts
+++ starter-code/src/app/contact-list/contact-list.component.ts
@@ -8,7 +8,7 @@ import contactList from '../contacts';
 })
 export class ContactListComponent implements OnInit {
   contacts: Object[];
-  newContact: Object = {};
+  newContact: any = {};
 
   constructor() { }
 
@@ -19,6 +19,12 @@ export class ContactListComponent implements OnInit {
   addContact(){
     console.log("Add contact has been called");
     // add contact to contacts list
+    const addContact = {name: this.newContact.name, email: this.newContact.email, phoneNumber: this.newContact.phoneNumber}
+    this.contacts.push(addContact);
     // clear inputs
+    this.newContact.name = "";
+    this.newContact.email = "";
+    this.newContact.phoneNumber = "";
+    this.newContact.image = "";
   }
 }
